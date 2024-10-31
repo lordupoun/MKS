2a99ae2c37744fcccf0ea8c10bfa2ac2d5e0d68b
lordSkola <vilda.broucek@gmail.com>
2024-10-31 07:31:38 +0100

    cv6

---
+++ cv6/Core/Inc/sct.h
@@ -0,0 +1,20 @@
+/*
+ * sct.h
+ *
+ *  Created on: Oct 10, 2024
+ *      Author: 240141
+ */
+#include "main.h"
+
+#ifndef INC_SCT_H_
+#define INC_SCT_H_
+
+void sct_init(void);
+
+void sct_led(uint32_t value);
+
+void sct_value(uint16_t value, uint8_t led);
+
+static const uint32_t reg_values[4][10];
+
+#endif /* INC_SCT_H_ */
